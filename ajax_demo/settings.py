84cccbfaab8e1eaf4eff453602a6bbb1265461ca
Chris <chris@strike.ly>
2015-10-11 20:14:53 +0100

    Update db names for livetest

--- ajax_demo/settings.py
+++ ajax_demo/settings.py
@@ -85,9 +85,9 @@ WSGI_APPLICATION = 'ajax_demo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ajaxtest',
-        'USER': 'ajaxtest',
-        'PASSWORD': 'ajaxtest',
+        'NAME': 'betterbusinesscase',
+        'USER': 'betterbusinesscase',
+        'PASSWORD': 'betterbusinesscase',
         'HOST': "localhost",  # Empty for localhost through domain sockets or
                               # '127.0.0.1' for localhost through TCP.
         'PORT': '',           # Set to empty string for default.
