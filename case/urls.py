52fc76e0e9a80dc1b9fe5527fe28d3b46d93f49c
Chris <chris@strike.ly>
2015-11-22 15:29:54 +0000

    Working version of options entry and appraisal/editing.  Needs styling.

--- case/urls.py
+++ case/urls.py
@@ -1,10 +1,18 @@
 __author__ = 'chris'
 from django.conf.urls  import patterns, include, url
+import views
 
 urlpatterns = patterns('',
-    url(r"^options/(?P<dimension>[^/]+)/$", "case.views.dimension"),
-    #url(r"^dimension_(?P<dimension_number>\d+)/$", "case.views.dimension"),
-    url(r"^scoring_(?P<score_number>\d+)/$", "case.views.scoring"),
+
+    url(r'^options/new/(?P<dimension>[^/]+)/$', views.options_new, name='options_new'),
+    url(r'^options/new/$', views.options_new, name='options_new'),
+    url(r'^options/(?P<pk>[0-9]+)/$', views.options_detail, name='options_detail'),
+    url(r'^options/(?P<pk>[0-9]+)/edit/$', views.options_edit, name='options_edit'),
+    url(r'^options/(?P<dimension>[^/]+)/$', 'case.views.options_main'),
+    url(r'^options/$', 'case.views.options_main'),
+    #url(r'^dimension_(?P<dimension_number>\d+)/$', 'case.views.dimension'),
+    url(r'^scoring_(?P<score_number>\d+)/$', 'case.views.scoring'),
+
     #url(r'^score/(?P<option>[^/]+)/$', ('case.views.scoring')),
-   # url(r'^', 'case.views.index'),
+    url(r'^', 'case.views.index'),
 )
--- case/views.py
+++ case/views.py
@@ -1,38 +1,59 @@
-from django.shortcuts import render_to_response
-
+from django.shortcuts import render_to_response, render, redirect, get_object_or_404
+from time import timezone
 # Create your views here.
 
 
-def dimension(request, dimension):
-    from django.shortcuts import get_object_or_404, render
-    from django.http import HttpResponseRedirect, HttpResponse
-    from django.core.urlresolvers import reverse
+def options_main(request, dimension=""):
+    from django.shortcuts import render
+    if not dimension:
+        return render(request, "case/options.html")
+    else:
+        from .models import Option
+        options = Option.objects.filter(option_type=dimension)
+        return render(request, "case/options.html", {"options": options, "dimension": dimension})
 
-    from .models import Option
-    def submit_option(request, dimension):
-        option = get_object_or_404(Option, option_type=dimension)
-        try:
-            selected_option = option.option_set.get(option_type=request.POST['dimension'],
-                                                    strength=request.POST['strength'],
-                                                    text=request.POST['text']
-                                                    )
-        except (KeyError, Option.DoesNotExist):
-            # Redisplay the question voting form.
-            return render(request, 'case/options.html', {
-                'dimension': dimension,
-                'error_message': "You didn't enter any text.",
-            })
+
+def options_new(request, dimension=""):
+    print "DEBUG DIMENSION", dimension
+    from .forms import OptionForm
+    if request.method == "POST":
+        form = OptionForm(request.POST)
+        option = form.save()
+        if form.is_valid():
+            option = form.save(commit=False)
+
+            option.save()
+    else:
+        if dimension:
+            form = OptionForm(initial={"option_type": dimension})
+            print "WITH DIMENSION"
         else:
-            option = Option.objects.create(option_type=request.POST['dimension'],
-                                           strength=request.POST['strength'],
-                                           text=request.POST['text']
-                                           )
-            # Always return an HttpResponseRedirect after successfully dealing
-            # with POST data. This prevents data from being posted twice if a
-            # user hits the Back button.
-            return HttpResponseRedirect(reverse(option, args=(option.option_type,)))
+            form = OptionForm
+            print "WITHOUT"
+    return render(request, 'case/option_edit.html', {'form': form,  "option": dimension})
 
-    return render_to_response("case/options.html", dimension)
+
+def options_edit(request, pk):
+    from .forms import OptionForm
+    from .models import Option
+    option = get_object_or_404(Option, pk=pk)
+    if request.method == "POST":
+        form = OptionForm(request.POST, instance=option)
+        if form.is_valid():
+            option = form.save(commit=False)
+            option.save()
+            print option.pk
+            return redirect('case:options_detail', pk=option.pk)
+    else:
+        form = OptionForm(instance=option)
+    return render(request, 'case/option_edit.html', {'form': form})
+
+
+def options_detail(request, pk):
+    from .models import Option
+    option = get_object_or_404(Option, pk=pk)
+    return render(request, 'case/option_detail.html', {'option': option})
+
 
 def scoring(request, score_number):
     """Toggle "like" for a single color, then refresh the color-list page."""
@@ -53,6 +74,7 @@ def scoring(request, score_number):
     """
     #Render the just-clicked-on like-button.
     return  render_to_response("case/scoring_" + score_number+".html")
+
 
 def index(request):
     return render_to_response('case/index.html')
