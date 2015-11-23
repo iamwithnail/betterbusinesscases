21f4a38322d369ff7711093187880e28d646e711
Chris <chris@strike.ly>
2015-11-23 20:10:28 +0000

    Fix to make css relative to local machine again!

--- ajax_demo/settings.py
+++ ajax_demo/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/color_liker/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = ''
 STATICFILES_DIRS = (
     #Put strings here, like "/home/html/static" or "C:/www/django/static".
